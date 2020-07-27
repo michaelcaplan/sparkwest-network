4a23ad09ca934bd52960ccbf9b29bcfca9e910d1
maxcaplan <maxacaplan@gmail.com>
2020-07-27 15:44:37 -0300

    sticky footer layout added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Nuxt />
+    <div id="content-wrapper">
+      <Navbar />
+
+      <Nuxt />
+
+      <div id="footer-push"></div>
+    </div>
+
     <Footer />
   </div>
 </template>
+
+<script>
+import Footer from '~/components/Footer'
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: { Navbar, Footer },
+}
+</script>
 
 <style>
 html,
@@ -40,8 +55,63 @@ h6 {
 }
 
 #app {
-  min-height: 100%;
+  height: 100vh;
   position: relative;
+}
+
+#content-wrapper {
+  min-height: 100%;
+  overflow-x: hidden;
+}
+
+@media only screen and (max-width: 576px) {
+  #content-wrapper {
+    margin-bottom: -740px;
+  }
+
+  #footer-push {
+    height: -740px;
+  }
+}
+
+@media only screen and (min-width: 576px) {
+  #content-wrapper {
+    margin-bottom: -585px;
+  }
+
+  #footer-push {
+    height: -585px;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  #content-wrapper {
+    margin-bottom: -500px;
+  }
+
+  #footer-push {
+    height: -500px;
+  }
+}
+
+@media only screen and (min-width: 992px) {
+  #content-wrapper {
+    margin-bottom: -375px;
+  }
+
+  #footer-push {
+    height: -375px;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  #content-wrapper {
+    margin-bottom: -351px;
+  }
+
+  #footer-push {
+    height: -351px;
+  }
 }
 
 .fr-box {
@@ -52,10 +122,3 @@ h6 {
   font-family: inherit;
 }
 </style>
-<script>
-import Footer from '~/components/Footer'
-import Navbar from '@/components/Navbar'
-export default {
-  components: { Navbar, Footer },
-}
-</script>
