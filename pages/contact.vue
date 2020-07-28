cd7d33551e499b6fb55b7cbffeb45c0682c9729a
maxcaplan <maxacaplan@gmail.com>
2020-07-27 22:36:20 -0300

    contact page layout

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,95 @@
 <template>
-  <h1>Contact</h1>
+  <div class="container py-3">
+    <h1 class="text-center">Contact Us</h1>
+
+    <hr />
+
+    <div class="row">
+      <div class="col-12 col-md mb-3 mb-md-0">
+        <p>
+          <b>Lets chat!</b> Have some questions or would like to give some
+          feedback? We would love to here from you!
+        </p>
+
+        <p>Email: <b>Info@sparkwest.network</b></p>
+
+        <div class="row">
+          <div class="col-auto">
+            <button class="btn btn-lg btn-primary">
+              <i class="fa fa-facebook-f"></i>
+            </button>
+          </div>
+          <div class="col-auto p-0">
+            <button class="btn btn-lg btn-info">
+              <i class="fa fa-twitter"></i>
+            </button>
+          </div>
+          <div class="col-auto">
+            <button class="btn btn-lg btn-danger">
+              <i class="fa fa-instagram"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md">
+        <form @submit.prevent="sendMessage">
+          <div class="form-group">
+            <label for="emailInput">Email</label>
+            <input
+              id="emailInput"
+              v-model="email"
+              type="email"
+              class="form-control"
+              placeholder="john@doe.ca"
+              required
+            />
+          </div>
+
+          <div class="form-group">
+            <label for="emailInput">Message</label>
+            <textarea
+              id="messageInput"
+              v-model="message"
+              class="form-control"
+              cols="30"
+              rows="6"
+              required
+            ></textarea>
+          </div>
+
+          <input
+            type="submit"
+            value="Send Message"
+            class="btn btn-success float-right"
+          />
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      email: '',
+      message: '',
+    }
+  },
+  methods: {
+    sendMessage() {
+      const messageTrim = this.message.trim()
+      window.location.href =
+        'mailto:info@sparkwest.network?subject=Contact%Form&body=' + messageTrim
+    },
+  },
+}
 </script>
+
+<style scoped>
+#messageInput {
+  resize: none;
+}
+</style>
