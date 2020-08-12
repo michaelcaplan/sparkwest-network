91b27368fd49ef106097bf7129968863e4148f67
maxcaplan <maxacaplan@gmail.com>
2020-08-12 16:42:14 -0300

    event liking cloud functions added

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,7 @@ admin.initializeApp({
 const bucket = admin.storage().bucket()
 const db = admin.firestore()
 
+// Handles all data related to user profile on deletion
 exports.deleteUserProfile = functions.auth.user().onDelete((user) => {
   const UID = user.uid
 
@@ -23,4 +24,103 @@ exports.deleteUserProfile = functions.auth.user().onDelete((user) => {
   Promise.all([deleteDoc, deleteAvatar]).catch((e) => {
     functions.logger.warn('Error deleting user profile', e)
   })
+})
+
+// Updates event document likes array when user likes event
+exports.likeEvent = functions.https.onCall(async (data, context) => {
+  try {
+    const UID = data.uid
+    const docID = data.docID
+
+    const docRef = db.collection('events').doc(docID)
+    const doc = await docRef.get()
+
+    // Check if event doc exists
+    if (doc.exists) {
+      const likes = doc.data().likes || []
+
+      // Check if user already liked event
+      if (likes.includes(UID)) {
+        throw new functions.https.HttpsError(
+          'invalid-argument',
+          'the user with UID: ' + UID + ' has already liked this event'
+        )
+      } else {
+        likes.push(UID)
+
+        await docRef.update({
+          likes: likes,
+        })
+
+        return {
+          message: 'user added to likes',
+          UID: UID,
+          docId: docID,
+          status: 200,
+          success: true,
+        }
+      }
+    } else {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'the document with ID: ' + docID + ' does not exist'
+      )
+    }
+  } catch (e) {
+    functions.logger.log('Error adding like', e)
+    throw new functions.https.HttpsError(
+      'server-error',
+      'there was an internal error while processing this request'
+    )
+  }
+})
+
+// Updates event document likes array when user unlikes event
+exports.unlikeEvent = functions.https.onCall(async (data, context) => {
+  try {
+    const UID = data.uid
+    const docID = data.docID
+
+    const docRef = db.collection('events').doc(docID)
+    const doc = await docRef.get()
+
+    // Check if event doc exists
+    if (doc.exists) {
+      const likes = doc.data().likes || []
+
+      // Check if user has liked event
+      if (!likes.includes(UID)) {
+        throw new functions.https.HttpsError(
+          'invalid-argument',
+          'the user with UID: ' + UID + " hasn't liked this event"
+        )
+      } else {
+        const index = likes.indexOf(UID)
+        likes.splice(index, 1)
+
+        await docRef.update({
+          likes: likes,
+        })
+
+        return {
+          message: 'user removed from likes',
+          UID: UID,
+          docId: docID,
+          status: 200,
+          success: true,
+        }
+      }
+    } else {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'the document with ID: ' + docID + ' does not exist'
+      )
+    }
+  } catch (e) {
+    functions.logger.log('Error adding like', e)
+    throw new functions.https.HttpsError(
+      'server-error',
+      'there was an internal error while processing this request'
+    )
+  }
 })
--- nuxt.config.js
+++ nuxt.config.js
@@ -138,6 +138,7 @@ export default {
           auth: true,
           firestore: true,
           storage: true,
+          functions: true,
         },
       },
     ],
